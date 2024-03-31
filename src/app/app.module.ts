f0c2db282b387af2c043fd046d2230ed4132e4d8
yasmine taous <yasmine.taous@gmail.com>
2024-03-31 01:39:32 +0100

    exercice 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Exercice1Component } from './exercice1/exercice1.component';
 import { Exercice2Component } from './exercice2/exercice2.component';
+import { Exercice3Component } from './exercice3/exercice3.component';
+import { Exercice4Component } from './exercice4/exercice4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Exercice2Component
+    Exercice1Component,
+    Exercice2Component,
+    Exercice3Component,
+    Exercice4Component,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exercice2',
   templateUrl: './exercice2.component.html',
-  styleUrls: ['./exercice2.component.css']
+  styleUrls: ['./exercice2.component.css'],
 })
 export class Exercice2Component implements OnInit {
+  color: String;
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  public clickExercice(event: Event): void {
+    console.log('click in square');
   }
-
 }
